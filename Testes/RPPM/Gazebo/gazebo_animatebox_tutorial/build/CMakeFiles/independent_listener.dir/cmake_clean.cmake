b0035e363a1c39d0cd030f4cc289dd2de3e92f04
Raphzor <raphaelppmello@gmail.com>
2014-08-27 19:54:36 -0300

    Testes com integração Gazebo/ROS

---
+++ Testes/RPPM/Gazebo/gazebo_animatebox_tutorial/build/CMakeFiles/independent_listener.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/independent_listener.dir/independent_listener.cc.o"
+  "independent_listener.pdb"
+  "independent_listener"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/independent_listener.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
